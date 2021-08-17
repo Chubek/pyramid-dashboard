7d33383d8e299c67aa1612cae8f8c1d4cfb23a42
Chubak <chubakbidpaa@gmail.com>
2021-08-18 01:13:34 +0430

    done with es

--- scripts/data/es.py
+++ scripts/data/es.py
@@ -1,1 +1,25 @@
 from elasticsearch import Elasticsearch
+from dotenv import dotenv_values
+from elasticsearch import helpers
+
+temp = dotenv_values(".env")
+
+es = Elasticsearch([temp['ES_HOST']], maxsize=25)
+
+
+def get_results(index_name, query_type, field_name, field_match):
+    return es.search(index=index_name, body={"query":{query_type:{field_name: field_match}}})
+
+
+def doc_generator(df, index_name):
+    df_iter = df.iterrows()
+    for index, document in df_iter:
+        yield {
+                "_index": index_name,
+                "_type": "_doc",
+                "_id" : index,
+                "_source": document.to_dict(),
+            }
+    return True
+
+insert_into_es = lambda df, index_name: helpers.bulk(es, doc_generator(df, index_name))
