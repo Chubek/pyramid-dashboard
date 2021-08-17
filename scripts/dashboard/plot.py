6b12750c53aaad0146f47561f3ef785944acec8e
Chubak <chubakbidpaa@gmail.com>
2021-08-17 20:41:25 +0430

    init

---
+++ scripts/dashboard/plot.py
@@ -0,0 +1,13 @@
+from typing import TextIO
+import plotly.express as px
+
+def construct_bar_chart(df, x, y, color, barmode, title):
+    return px.bar(df, x=x, y=y, color=color, barmode=barmode, title=title)
+
+
+def construct_line_chart(df, x, y, color, title):
+    return px.line(df, x=x, y=y, color=color, title=title)
+
+
+def construct_scatter_chart(df, x, y, title):
+    return px.scatter(df, x=x, y=y, title=title)
