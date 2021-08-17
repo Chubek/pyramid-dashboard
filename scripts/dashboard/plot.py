1f0fb228c933813d67cdda35b0474e7a884f424e
Chubak <chubakbidpaa@gmail.com>
2021-08-17 23:00:10 +0430

    es start

--- scripts/dashboard/plot.py
+++ scripts/dashboard/plot.py
@@ -11,3 +11,8 @@ def construct_line_chart(df, x, y, color, title):
 
 def construct_scatter_chart(df, x, y, title):
     return px.scatter(df, x=x, y=y, title=title)
+
+def construct_histogram(df, x, title):
+    return px.histogram(df, x=x, title=title)
+
+
